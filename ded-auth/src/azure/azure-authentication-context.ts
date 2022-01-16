2689c777cbf69252502cb9421cb0c442e3cd2ab8
dionysus <dionysus@isitymarket.onmicrosoft.com>
2022-01-16 13:49:20 -0500

    azure auth context commit

--- ded-auth/src/azure/azure-authentication-context.ts
+++ ded-auth/src/azure/azure-authentication-context.ts
@@ -1,3 +1,4 @@
+//import {AzureAuthenticationContext} from './azure-authentication-context'
 import {MSAL_CONFIG} from './azure-authentication-config'
 import
 { PublicClientApplication,
@@ -8,5 +9,88 @@ import
   PopupRequest,
 } from "@azure/msal-browser";
 
-export
- cl
+export class AzureAuthenticationContext {
+	private MYMSALObj: PublicClientApplication = new PublicClientApplication(
+		MSAL_CONFIG
+	);
+	private account?: AccountInfo;
+	private loginRedirectRequest?: RedirectRequest;
+	private loginRequest?: PopupRequest;
+
+	public isAuthticationConfigured = false;
+
+	constructor() {
+		// @ts-ignore
+		this.account = null;
+		this.setRequestObjects();
+		if (MSAL_CONFIG?.auth?.clientId) {
+			this.isAuthticationConfigured = true;
+		}
+	}
+	private setRequestObjects(): void {
+		this.loginRequest = {
+			scopes: [],
+			prompt: "select_account",
+		};
+
+		this.loginRedirectRequest = {
+			...this.loginRequest,
+			redirectsStartPage: window.location.href,
+		};
+	}
+
+	login(signInType: string, setUser: any): void {
+		if (signInType === "loginPopup") {
+			this.MYMSALObj.loginPopup(this.loginRequest)
+				.then((resp: AuthenticationResult) => {
+					this.handleResponse(resp, setUser);
+				})
+				.catch((err) => {
+					console.error(err);
+				});
+		} else if (signInType === "loginRedirect") {
+			this.MYMSALObj.loginRedirect(this.loginRedirectRequest);
+		}
+	}
+
+	logout(account: AccountInfo): void {
+		const logOutRequest: EndSessionRequest = {
+			account,
+		};
+
+		this.MYMSALObj.logout(logOutRequest);
+	} //Deprecated logout function. Use logoutRedirect or logoutPopup instead
+	handleResponse(response: AuthenticationResult, incomingFunction: any) {
+		if (response !==null && response.account !==null) {
+			this.account = response.account;
+		} else {
+			this.account = this.getAccount ();
+		}
+
+		if (this.account) {
+			incomingFunction(this.account);
+		}
+	}
+	private getAccount(): AccountInfo | undefined {
+		console.log('loadAuthModeule');
+		const currentAccounts = this.MYMSALObj.getAllAccounts();
+		if (currentAccounts === null) {
+			//@ts-ignore
+			console.log("No accounts detected");
+			return undefined;
+		}
+
+		if (currentAccounts.length > 1) {
+			//TBD: add choose acct code here
+			// @ts-ignore
+			console.log(
+				"multiple accounts detected, need to add choose account code."
+			);
+			return currentAccounts[0];
+		}	else if (currentAccounts.length === 1) {
+			return currentAccounts[0];
+		}
+	}
+};
+
+export default AzureAuthenticationContext;
