49997a34b875ef1f3400f3bbe79605aada9995ce
dionysus <dionysus@isitymarket.onmicrosoft.com>
2022-01-16 09:57:00 -0500

    authentication files

---
+++ ded-auth/src/azure/azure-authentication-context.ts
@@ -0,0 +1,12 @@
+import {MSAL_CONFIG} from './azure-authentication-config'
+import
+{ PublicClientApplication,
+	AuthenticationResult,
+	AccountInfo,
+	EndSessionRequest,
+  RedirectRequest,
+  PopupRequest,
+} from "@azure/msal-browser";
+
+export
+ cl
