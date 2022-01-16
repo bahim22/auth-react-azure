5d0f73390e43b6d29ae3a0d8410c61a25e90dcc9
dionysus <dionysus@isitymarket.onmicrosoft.com>
2022-01-16 16:04:51 -0500

    compo con't

--- ded-auth/src/azure/azure-authentication-component.tsx
+++ ded-auth/src/azure/azure-authentication-component.tsx
@@ -1,8 +1,17 @@
 import React, { useState } from "react"
 import AzureAuthenticationContext from "./azure-authentication-context"
 import { AccountInfo } from "@azure/msal-browser"
+import { JsxEmit } from "typescript";
 
 const ua = window.Navigator.userAgent;
 const msie = ua.indexOf ("MSIE ");
-const msie11
-const isIE
+const msie11 = ua.indexOf ("Trident/ ");
+const isIE = msie > 0 || msie11 > 0;
+
+//Login & logout button
+const AzureAuthenticationButton = ({onAuthenticated }: any): JSX.Element => {
+	//Az client context
+	const authenticationModule: AzureAuthenticationContext = new AzureAuthenticationContext();
+	const [authenticated, setAuthenticated] = useState<Boolean>(false);
+	const [user, setUser] = useState<AccountInfo>();
+}
