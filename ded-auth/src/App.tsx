f42034e7f0550b7330f1bc6ee131b840df53395d
dionysus <dionysus@isitymarket.onmicrosoft.com>
2022-01-16 23:38:20 -0500

    final app.tsx function edit; pre build

--- ded-auth/src/App.tsx
+++ ded-auth/src/App.tsx
@@ -1,8 +1,81 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from "react";
+//import AzureAuthenticationContext from "./azure-authentication-context";
+import { AccountInfo } from "@azure/msal-browser";
+//import { json } from 'stream/consumers';
+import AzureAuthenticationButton from './azure/azure-authentication-component';
+
 function App() {
+	//current authenticated User
+	const [currentUser, setCurrentuser] = useState<AccountInfo>();
+
+	//auth callback
+	const onAuthenticated = async (userAccountInfo: AccountInfo) => {
+		setCurrentuser(userAccountInfo);
+	};
+
+	//render json data in readable format
+	const PrettyPrintJson = ({ data }: any) => {
+		return (
+			<div>
+				<pre>{JSON.stringify(data, null, 2)}</pre>
+			</div>
+		);
+	};
+	// Quick link - user revokes app's permission
+	const ShowPermissionRevokeLinks = () => {
+		return (
+			<div>
+				<div>
+					<a
+						href="https://myapps.microsoft.com"
+						target="_blank"
+						rel="noreferrer">
+						Revoke AAD permission
+					</a>
+				</div>
+				<div>
+					<a
+						href="https://account.live.com/consent/manage"
+						target="_blank"
+						rel="noreferrer">
+						Revoke Consumer permission
+					</a>
+				</div>
+			</div>
+		);
+	};
+
+	return (
+		<div id="App"  className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to  <code>HimaLand:</code> Dionysus Era
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/bahim22"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub Profile
+        </a>
+      </header>
+			<h2>Login to Continue</h2>
+			<AzureAuthenticationButton onAuthenticated={onAuthenticated} />
+			{currentUser && (
+				<div>
+					<PrettyPrintJson data={currentUser} />
+					<ShowPermissionRevokeLinks />
+				</div>
+			)}
+		</div>
+	)
+}
+/*function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +94,6 @@ function App() {
       </header>
     </div>
   );
-}
+}*/
 
 export default App;
--- ded-auth/src/azure/azure-authentication-component.tsx
+++ ded-auth/src/azure/azure-authentication-component.tsx
@@ -1,17 +1,74 @@
 import React, { useState } from "react"
 import AzureAuthenticationContext from "./azure-authentication-context"
 import { AccountInfo } from "@azure/msal-browser"
-import { JsxEmit } from "typescript";
+//import { JsxEmit } from "typescript";
 
-const ua = window.Navigator.userAgent;
+const ua = window.navigator.userAgent;
 const msie = ua.indexOf ("MSIE ");
 const msie11 = ua.indexOf ("Trident/ ");
 const isIE = msie > 0 || msie11 > 0;
 
 //Login & logout button
-const AzureAuthenticationButton = ({onAuthenticated }: any): JSX.Element => {
+const AzureAuthenticationButton = ({ onAuthenticated }: any): JSX.Element => {
 	//Az client context
 	const authenticationModule: AzureAuthenticationContext = new AzureAuthenticationContext();
 	const [authenticated, setAuthenticated] = useState<Boolean>(false);
 	const [user, setUser] = useState<AccountInfo>();
-}
+
+	const logIn = (method: string): any => {
+		const typeName = "loginPopup";
+		const logInType = isIE ? "loginRedirect" : typeName;
+
+		//Az login
+		authenticationModule.login(logInType, returnedAccountInfo);
+	};
+	const logOut = (): any => {
+		if (user) {
+			onAuthenticated(undefined);
+			// Az logout
+			authenticationModule.logout(user);
+		}
+	};
+	const returnedAccountInfo = (user: AccountInfo) => {
+		//set state
+		setAuthenticated(user?.name ? true : false);
+		onAuthenticated(user);
+		setUser(user);
+	};
+
+	const showLogInButton = (): any => {
+		return (
+			<button id="authenticationButton" onClick={() => logIn("loginPopup")}>
+				Log in
+			</button>
+		);
+	};
+
+	const showLogOutButton = (): any => {
+		return (
+			<div id="authenticationButtonDiv">
+				<div id="authentication">
+					<button id="authenticationButton" onClick={() => logOut ()}>
+						Log Out
+					</button>
+				</div>
+			</div>
+		)
+	};
+
+	const showButton = (): any => {
+		return authenticated ? showLogOutButton : showLogInButton();
+	};
+
+	return (
+		<div id="authentication">
+			{authenticationModule.isAuthticationConfigured ? (
+				showButton()
+			) : (
+				<div>Authentication Client ID not Configured.</div>
+			)}
+		</div>
+	)
+};
+
+export default AzureAuthenticationButton;
